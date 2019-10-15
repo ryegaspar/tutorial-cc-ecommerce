3fe22d6902d6f1869fbc2b2c5326974fae77286a
rye gaspar <gaspar.ryan@outlook.com>
2019-10-14 19:52:03 -0500

    008 - simple products

---
+++ tests/Unit/Products/ProductTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit\Products;
+
+use App\Models\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProductTest extends TestCase
+{
+    /** @test */
+    public function it_uses_the_slug_for_the_route_key_name()
+    {
+        $product = new Product();
+
+        $this->assertEquals($product->getRouteKeyName(), 'slug');
+    }
+}
