1e9ba0c51f95a8e619ef6e51b1a6d1c130fd8f1d
rye gaspar <gaspar.ryan@outlook.com>
2019-10-17 20:17:35 -0500

    018 - fixing failing tests for the product index

--- app/Scoping/Scoper.php
+++ app/Scoping/Scoper.php
@@ -5,6 +5,7 @@ namespace App\Scoping;
 use App\Scoping\Contracts\Scope;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class Scoper
 {
@@ -17,13 +18,22 @@ class Scoper
 
     public function apply(Builder $builder, array $scopes)
     {
-        foreach($scopes as $key => $scope) {
-            if (!$scope instanceof Scope) {
+        foreach ($this->limitScopes($scopes) as $key => $scope) {
+            if (! $scope instanceof Scope) {
                 continue;
             }
+
             $scope->apply($builder, $this->request->get($key));
         }
 
         return $builder;
+    }
+
+    protected function limitScopes(array $scopes)
+    {
+        return Arr::only(
+            $scopes,
+            array_keys($this->request->all())
+        );
     }
 }
