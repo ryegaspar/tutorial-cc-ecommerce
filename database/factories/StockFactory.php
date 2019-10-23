4c0bbca81aa7b8c50cc33add5c4d48fdf1adf692
rye gaspar <gaspar.ryan@outlook.com>
2019-10-22 21:02:53 -0500

    026 - product stock blocks

---
+++ database/factories/StockFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use App\Models\Stock;
+use Faker\Generator as Faker;
+
+$factory->define(Stock::class, function (Faker $faker) {
+    return [
+        'quantity' => 1
+    ];
+});
