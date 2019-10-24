538ec59f47ef2097d2183ece02bbb8fb9e36c5ae
rye gaspar <gaspar.ryan@outlook.com>
2019-10-23 19:50:57 -0500

    032 - updating dropdown with stock count

--- client/pages/products/_slug.vue
+++ client/pages/products/_slug.vue
@@ -16,6 +16,12 @@
 
                         <hr>
 
+                        <span class="tag is-rounded is-medium is-dark"
+                              v-if="!product.in_stock"
+                        >
+                            Out of Stock
+                        </span>
+
                         <span class="tag is-rounded is-medium">
                             {{ product.price }}
                         </span>
@@ -36,8 +42,15 @@
                             >
                                 <div class="control">
                                     <div class="select is-fullwidth">
-                                        <select name="" id="">
-                                            <option value="">1</option>
+                                        <select name=""
+                                                id=""
+                                                v-model="form.quantity"
+                                        >
+                                            <option :value="x"
+                                                    v-for="x in parseInt(form.variation.stock_count)"
+                                                    :key="x">
+                                                {{ x }}
+                                            </option>
                                         </select>
                                     </div>
                                 </div>
@@ -66,6 +79,12 @@
     				variation: null,
                     quantity: 1
                 }
+            }
+        },
+
+        watch: {
+    		'form.variation' () {
+    			this.form.quantity = 1;
             }
         },
 
