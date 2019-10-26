9e206a92101515468b302a772c5328205a6a086f
rye gaspar <gaspar.ryan@outlook.com>
2019-10-25 22:19:23 -0500

    041 - the user cart relationship

--- tests/Unit/Models/Users/UserTest.php
+++ tests/Unit/Models/Users/UserTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit\Models\Users;
 
+use App\Models\ProductVariation;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -17,5 +18,32 @@ class UserTest extends TestCase
         ]);
 
         $this->assertNotEquals($user->password, 'cats');
+    }
+
+    /** @test */
+    public function it_has_many_cart_products()
+    {
+        $user = factory(User::class)->create();
+
+        $user->cart()->attach(
+            factory(ProductVariation::class)->create()
+        );
+
+        $this->assertInstanceOf(ProductVariation::class, $user->cart->first());
+    }
+
+    /** @test */
+    public function it_has_a_quantity_for_each_cart_product()
+    {
+        $user = factory(User::class)->create();
+
+        $user->cart()->attach(
+            factory(ProductVariation::class)->create(),
+            [
+                'quantity' => $quantity = 5
+            ]
+        );
+
+        $this->assertEquals($user->cart->first()->pivot->quantity, $quantity);
     }
 }
