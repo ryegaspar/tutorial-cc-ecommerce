1771ae259cf022b32310b877b3088cd4817cd16b
rye gaspar <gaspar.ryan@outlook.com>
2019-10-25 23:04:13 -0500

    042 - adding items to the cart

---
+++ tests/Unit/Cart/CartTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit\Cart;
+
+use App\Cart\Cart;
+use App\Models\ProductVariation;
+use App\Models\User;
+use Tests\TestCase;
+
+class CartTest extends TestCase
+{
+    /** @test */
+    public function it_can_add_products_to_the_cart()
+    {
+        $cart = new Cart(
+            $user = factory(User::class)->create()
+        );
+
+        $product = factory(ProductVariation::class)->create();
+
+        $cart->add([
+            ['id' => $product->id, 'quantity' => 1]
+        ]);
+
+        $this->assertCount(1, $user->fresh()->cart);
+    }
+}
