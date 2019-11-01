93627720e5ad068c0b1f3407bb94edff9d427708
rye gaspar <gaspar.ryan@outlook.com>
2019-11-01 08:52:15 -0500

    074 - hooking up shipping methods to countries

---
+++ tests/Unit/Models/Countries/CountryTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit\Models\Countries;
+
+use App\Models\Country;
+use App\Models\ShippingMethod;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CountryTest extends TestCase
+{
+    /** @test */
+    public function it_has_many_shipping_methods()
+    {
+        $country = factory(Country::class)->create();
+
+        $country->shippingMethods()->attach(
+            factory(ShippingMethod::class)->create()
+        );
+
+        $this->assertInstanceOf(ShippingMethod::class, $country->shippingMethods->first());
+    }
+}
--- tests/Unit/Models/ShippingMethods/ShippingMethodTest.php
+++ tests/Unit/Models/ShippingMethods/ShippingMethodTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Models\ShippingMethods;
 
 use App\Cart\Money;
+use App\Models\Country;
 use App\Models\ShippingMethod;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -10,6 +11,18 @@ use Tests\TestCase;
 
 class ShippingMethodTest extends TestCase
 {
+    /** @test */
+    public function it_belongs_to_many_countries()
+    {
+        $shipping = factory(ShippingMethod::class)->create();
+
+        $shipping->countries()->attach(
+            factory(Country::class)->create()
+        );
+
+        $this->assertInstanceOf(Country::class, $shipping->countries->first());
+    }
+
     /** @test */
     public function it_returns_a_money_instance_for_the_price()
     {
