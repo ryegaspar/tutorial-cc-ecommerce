58b69267e30cba3ba653bab672d46cd9bdbd1340
rye gaspar <gaspar.ryan@outlook.com>
2019-11-01 18:33:04 -0500

    083 - basic order validation

---
+++ tests/Feature/Orders/OrderStoreTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Tests\Feature\Orders;
+
+use App\Models\Address;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class OrderStoreTest extends TestCase
+{
+    /** @test */
+    public function it_fails_if_not_authenticated()
+    {
+        $this->json('POST', 'api/orders')
+            ->assertStatus(401);
+    }
+
+    /** @test */
+    public function it_requires_an_address()
+    {
+        $user = factory(User::class)->create();
+
+        $this->jsonAs($user, 'POST', 'api/orders')
+            ->assertJsonValidationErrors(['address_id']);
+    }
+
+    /** @test */
+    public function it_requires_an_address_that_exists()
+    {
+        $user = factory(User::class)->create();
+
+        $this->jsonAs($user, 'POST', 'api/orders',
+            [
+                'address_id' => 1
+            ])
+            ->assertJsonValidationErrors(['address_id']);
+    }
+
+    /** @test */
+    public function it_requires_an_address_that_belongs_to_the_authenticated_user()
+    {
+        $user = factory(User::class)->create();
+
+        $address = factory(Address::class)->create([
+            'user_id' => factory(User::class)->create()->id
+        ]);
+
+        $this->jsonAs($user, 'POST', 'api/orders',
+            [
+                'address_id' => $address->id
+            ])
+            ->assertJsonValidationErrors(['address_id']);
+    }
+
+    /** @test */
+    public function it_requires_a_shipping_method()
+    {
+        $user = factory(User::class)->create();
+
+        $this->jsonAs($user, 'POST', 'api/orders')
+            ->assertJsonValidationErrors(['shipping_method_id']);
+    }
+
+    /** @test */
+    public function it_requires_a_shipping_method_that_exists()
+    {
+        $user = factory(User::class)->create();
+
+        $this->jsonAs($user, 'POST', 'api/orders',
+            [
+                'shipping_method_id' => 1
+            ])
+            ->assertJsonValidationErrors(['shipping_method_id']);
+    }
+}
