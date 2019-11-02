e3643f8c98ecf150afbad28981747ba9997bc997
rye gaspar <gaspar.ryan@outlook.com>
2019-11-02 15:34:43 -0500

    088 - attaching products when ordering

--- tests/Feature/Orders/OrderStoreTest.php
+++ tests/Feature/Orders/OrderStoreTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Feature\Orders;
 
 use App\Models\Address;
+use App\Models\ProductVariation;
 use App\Models\ShippingMethod;
+use App\Models\Stock;
 use App\Models\User;
 use Tests\TestCase;
 
@@ -112,6 +114,40 @@ class OrderStoreTest extends TestCase
                 'address_id'         => $address->id,
                 'shipping_method_id' => $shipping->id
             ]);
+    }
+
+    /** @test */
+    public function it_attaches_the_products_to_the_order()
+    {
+        $user = factory(User::class)->create();
+
+        $user->cart()->sync(
+            $product = $this->productWithStock()
+        );
+
+        list($address, $shipping) = $this->orderDependencies($user);
+
+        $response = $this->jsonAs($user, 'POST', 'api/orders',
+            [
+                'address_id'         => $address->id,
+                'shipping_method_id' => $shipping->id
+            ]);
+
+        $this->assertDatabaseHas('product_variation_order',
+            [
+                'product_variation_id' => $product->id,
+            ]);
+    }
+
+    protected function productWithStock()
+    {
+        $product = factory(ProductVariation::class)->create();
+
+        factory(Stock::class)->create([
+            'product_variation_id' => $product->id
+        ]);
+
+        return $product;
     }
 
     protected function orderDependencies(User $user)
