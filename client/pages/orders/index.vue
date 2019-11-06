050fc2fc4d0c39e25c10120a3900d99435c035fb
rye gaspar <gaspar.ryan@outlook.com>
2019-11-06 06:16:33 -0600

    100 - order index setup

--- client/pages/orders/index.vue
+++ client/pages/orders/index.vue
@@ -1,7 +1,34 @@
 <template>
-    <p>order</p>
+    <div class="section">
+        <div class="container is-fluid">
+            <div class="columns">
+                <div class="column is-12">
+                    <h1 class="title is-4">Your orders</h1>
+
+                    <article class="message">
+                        <div class="message-body">
+                            <table class="table is-hoverable is-fullwidth">
+                                <tbody>
+                                    <order></order>
+                                </tbody>
+                            </table>
+                        </div>
+                    </article>
+                    <p>
+                        You have no orders
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-    export default {}
+    import Order from "@/components/orders/Order";
+
+	export default {
+		components: {
+			Order
+        }
+    }
 </script>
