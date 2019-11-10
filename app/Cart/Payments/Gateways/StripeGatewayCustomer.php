0b88b9ced6d4304f87db6e0185598855363a2ef1
rye gaspar <gaspar.ryan@outlook.com>
2019-11-09 22:35:49 -0600

    113 - storing a payment method

--- app/Cart/Payments/Gateways/StripeGatewayCustomer.php
+++ app/Cart/Payments/Gateways/StripeGatewayCustomer.php
@@ -2,11 +2,22 @@
 
 namespace App\Cart\Payments\Gateways;
 
+use App\Cart\Payments\Gateway;
 use App\Cart\Payments\GatewayCustomer;
 use App\Models\PaymentMethod;
+use Stripe\Customer as StripeCustomer;
 
 class StripeGatewayCustomer implements GatewayCustomer
 {
+    protected $gateway;
+    protected $customer;
+
+    public function __construct(Gateway $gateway, StripeCustomer $customer)
+    {
+        $this->gateway = $gateway;
+        $this->customer = $customer;
+    }
+
     public function charge(PaymentMethod $card, $amount)
     {
         // TODO: Implement charge() method.
@@ -14,6 +25,23 @@ class StripeGatewayCustomer implements GatewayCustomer
 
     public function addCard($token)
     {
-        // TODO: Implement addCard() method.
+        $card = $this->customer->sources->create([
+            'sources'        => $token
+        ]);
+
+        $this->customer->default_source = $card->id;
+        $this->customer->save();
+
+        $this->gateway->user()->paymentMethods()->create([
+            'provider_id' => $card->id,
+            'card_type'   => $card->brand,
+            'last_four'   => $card->last4,
+            'default'     => true,
+        ]);
+    }
+
+    public function id()
+    {
+        return $this->customer->id;
     }
 }
